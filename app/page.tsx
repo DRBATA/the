3e66d578ee2a9bc1c3654d9eef2c519311a3f874
DRBATA <164792895+DRBATA@users.noreply.github.com>
2025-05-03 19:13:31 +0100

    fix: correct MagicLinkLogin import and vercelignore path

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import MagicLinkLogin from "../../components/MagicLinkLogin";
+import MagicLinkLogin from "../components/MagicLinkLogin";
 
 export default function Home() {
   return (
